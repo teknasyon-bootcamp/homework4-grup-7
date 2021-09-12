44f86d511bca40cd9da815856b8c456867f6c3db
ancyra2 <duran.talih@gmail.com>
2021-09-12 09:37:05 +0300

    Arayüz ve veri tabanı güncellendi.

--- index.php
+++ index.php
@@ -7,7 +7,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Posts</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-   
+    <style>
+      .post-box{
+        width: 100%;
+        height: 400px;
+        background-color: white;
+        border-radius: 5px;
+        position: relative;
+      }
+      .post-box h1{
+        text-align: center;
+      }
+      .post-box-inner p{
+        padding: 2em;
+      }
+      .post-box-inner a{
+        text-decoration: none;
+      }
+      .post-author{
+        position: absolute;
+        bottom:5%;
+        left: 3%;
+        font-weight: bold;
+      }
+      .post-read{
+        position: absolute;
+        bottom: 5%;
+        right:5%;
+      }
+    </style>
 </head>
 <body style = "background-color:rgb(240, 240, 240)">
    
@@ -57,7 +85,7 @@
           echo "<tr>" . PHP_EOL;
           echo "<th scope ='row'>$post_val[id]</th>" . PHP_EOL;
           echo "<td>$post_val[author]</td>" . PHP_EOL;
-          echo "<td><a href='http://localhost/homework4-grup7/$post_val[post_header]'> $post_val[post_header]</a></td>" . PHP_EOL;
+          echo "<td><a href='http://localhost/homework4-grup7/$post_val[post_header].php'> $post_val[post_header]</a></td>" . PHP_EOL;
           echo "<td>$post_val[post_created]</td>" . PHP_EOL;
           echo "</tr> " . PHP_EOL;
           
@@ -71,7 +99,7 @@
           echo "<tr>" . PHP_EOL;
           echo "<th scope ='row'>$post[id]</th>" . PHP_EOL;
           echo "<td>$post[author]</td>" . PHP_EOL;
-          echo "<td><a href='http://localhost/homework4-grup7/$post[post_header]'> $post[post_header]</a></td>" . PHP_EOL;
+          echo "<td><a href='http://localhost/homework4-grup7/$post[post_header].php'> $post[post_header]</a></td>" . PHP_EOL;
           echo "<td>$post[post_created]</td>" . PHP_EOL;
           echo "</tr> " . PHP_EOL;
       }
@@ -89,6 +117,45 @@
 <button class="btn btn-primary" type="submit">Ara</button>
 </form>
  
+<div class="container p-0 mt-5">
+
+    <?php 
+  if(isset($_GET['post'])){
+    foreach($post as $post_val){
+
+      echo "<div class ='post-box mt-5'>";
+      echo "<div class = 'post-box-inner'>";
+      echo "<h1> <a href='http://localhost/homework4-grup7/$post_val[post_header].php'>$post_val[post_header]</a></h1>";
+      echo "<p>";
+      echo substr($post_val['post_detail'], 0 ,1500);
+      echo "...</p>"; 
+      echo "</div>";
+      echo "<div class ='post-author'>Yazar: $post_val[author]</div>";
+      echo "<div class ='post-read'><a href='http://localhost/homework4-grup7/$post_val[post_header].php'>Devamını okumak için tıklayın</a></div>";
+      echo "</div>";
+    }
+
+  }else{
+
+    foreach($posts as $post){
+      echo "<div class ='post-box mt-5'>";
+      echo "<div class = 'post-box-inner'>";
+      echo "<h1> <a href='http://localhost/homework4-grup7/$post[post_header].php'>$post[post_header]</a></h1>";
+      echo "<p>";
+      echo substr($post['post_detail'], 0 ,1500);
+      echo "...</p>"; 
+      echo "</div>";
+      echo "<div class ='post-author'>Yazar: $post[author]</div>";
+      echo "<div class ='post-read'><a href='http://localhost/homework4-grup7/$post[post_header].php'>Devamını okumak için tıklayın</a></div>";
+      echo "</div>";
+     
+   }
+
+  }
+ 
+    ?>
+     
+</div>
     
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
